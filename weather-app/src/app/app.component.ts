c41e1b48d4f2ed348b149957fc5f3eecd16e9b29
Karoline Cruz <karollcruz@hotmail.com>
2024-02-13 02:13:40 +0000

    docs: container presentation pattern implementation

--- weather-app/src/app/app.component.ts
+++ weather-app/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CardContentComponent } from './components/card-content/card-content.component';
+import { CardContentComponent } from './container/card-content/card-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, CardContentComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
