a523f7613fe7b49035a9c1b7ffa7894d7894acaa
Karoline Cruz <karollcruz@hotmail.com>
2024-02-13 16:16:53 +0000

    docs: code formatting

--- weather-app/src/app/container/card-content/card-content.component.ts
+++ weather-app/src/app/container/card-content/card-content.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
-import { TodaysForecastComponent } from '../../components/todays-forecast/todays-forecast.component';
-import { WeeklyForecastComponent } from '../../components/weekly-forecast/weekly-forecast.component';
-import { ForecastHighlightsComponent } from '../../components/forecast-highlights/forecast-highlights.component';
-import { ForecastService } from '../../services/forecast.service';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable, catchError } from 'rxjs';
+import { ForecastService } from '../../core/services/forecast.service';
 import { TodaysForecast } from '../../interfaces/todays-forecast';
+
+import { TodaysForecastComponent } from '../../components/todays-forecast/todays-forecast.component';
+import { ForecastHighlightsComponent } from '../../components/forecast-highlights/forecast-highlights.component';
+import { WeeklyForecastComponent } from '../../components/weekly-forecast/weekly-forecast.component';
+import { ErrorMessageComponent } from '../../components/error-message/error-message.component';
 
 @Component({
   selector: 'card-content',
@@ -15,6 +17,7 @@ import { TodaysForecast } from '../../interfaces/todays-forecast';
     TodaysForecastComponent,
     WeeklyForecastComponent,
     ForecastHighlightsComponent,
+    ErrorMessageComponent,
   ],
   providers: [],
   templateUrl: './card-content.component.html',
@@ -22,9 +25,15 @@ import { TodaysForecast } from '../../interfaces/todays-forecast';
 })
 export class CardContentComponent implements OnInit {
   public forecastData$!: Observable<TodaysForecast>;
+  public errorMessage!: string;
   constructor(private service: ForecastService) {}
 
   ngOnInit(): void {
-    this.forecastData$ = this.service.getForecastData();
+    this.forecastData$ = this.service.getForecastData().pipe(
+      catchError((error: string) => {
+        this.errorMessage = error;
+        return EMPTY;
+      })
+    );
   }
 }
